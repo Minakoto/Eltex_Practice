06dc71b43877398d5f31124b2a47b411f4bc1a1e
linomary <linomary@student.21-school.ru>
2025-06-01 13:42:21 +0700

    Task 4

---
+++ module5/04/head.h
@@ -0,0 +1,17 @@
+
+#include <linux/cdev.h> 
+#include <linux/delay.h> 
+#include <linux/device.h> 
+#include <linux/fs.h> 
+#include <linux/init.h> 
+#include <linux/irq.h> 
+#include <linux/kernel.h> 
+#include <linux/module.h> 
+#include <linux/poll.h> 
+
+
+/*  Prototypes - this would normally go in a .h file */ 
+static int device_open(struct inode *, struct file *); 
+static int device_release(struct inode *, struct file *); 
+static ssize_t device_read(struct file *, char __user *, size_t, loff_t *); 
+static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *); 
